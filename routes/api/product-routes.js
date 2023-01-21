e4c794f7146a4f785f2a9f49b7a0f50010c8efa5
David McWhirter <david@tgsummit.com>
2023-01-21 13:17:47 -0500

    added CRUD methods

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+      Product.findAll({
+        include: [
+          Category,
+          {
+            model: Tag,
+            through: ProductTag,
+          },
+        ],
+      })
+        .then((products) => res.json(products))
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
